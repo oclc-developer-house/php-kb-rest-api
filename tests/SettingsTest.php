2d8bb975ee4f63f8bb592eb96c09300d6b6a45ca
Brian Cassidy <bricas@cpan.org>
2014-02-04 12:31:48 -0400

    getting started with phpunit

---
+++ tests/SettingsTest.php
@@ -0,0 +1,11 @@
+<?php
+
+class SettingsTest extends PHPUnit_Framework_TestCase {
+
+    public function testConstructor() {
+        // Just checking to see if phpunit is working
+        $stack = array();
+        $this->assertEquals(0, count($stack));
+    }
+
+}
