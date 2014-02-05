57587303c137a403f9c891f6107b786d4c76eb12
Karen A. Coombs <librarywebchic@gmail.com>
2014-02-05 00:07:11 -0500

    Base Resource Class and Tests

---
+++ src/Resource.php
@@ -0,0 +1,594 @@
+<?php
+
+use Guzzle\Http\Client;
+use Guzzle\Plugin\History\HistoryPlugin;
+use Guzzle\Plugin\Mock\MockPlugin;
+
+Class Resource
+{
+	/**
+	 * A generic class that represents an OCLC Resource.
+     * Optionally, it can include principal information in the form of a principal ID and IDNS that represent an user and a redirect URI to be used in the OAuth 2 login flows.
+     * The Resource class is used to
+     * - construct a Resource
+     * - read a Resource
+     * - create a Resource
+     * - update a Resource
+     * - delete a Resource
+     *
+     * @author Karen A. Coombs <coombsk@oclc.org>
+	 
+	 * @var string $service_url
+	 * @var string $object_path
+	 * @var binary $dataURLsyntax
+	 * @var string $nsURL
+	 * @var array $supportedAuthenticationMethods
+	 * @var string $wskey
+	 * @var string $accessToken
+	 * @var \OCLC\User $user an /OCLC/User object which contains a valid principalID, principalIDNS and institution ID for a user
+	 * @var string $id
+	 * @var array $requestParameters
+	 * @var string $request_url
+	 * @var string $headers
+	 * @var string $acceptType
+	 * @var string $mockResponseFilePath
+	 * @var string $responseCode
+	 * @var binary $responseOk
+	 * @var string $eTag
+	 * @var string $responseBody
+	 * @var string $doc
+	 * @var string $atomTitle
+	 * @var string $atomLink
+	 * @var string $errorCode
+	 * @var string $errorMessage
+	 * @var string $errorDetail
+	 * 
+	 */
+	
+	public static $service_url;
+	public static $object_path;
+	public static $dataURLsyntax = true;
+	public static $nsURL;
+	public static $supportedAuthenticationMethods = array('HMAC', 'AccessToken');
+	
+	protected $wskey;
+	protected $accessToken = null;
+	protected $user = null;
+	
+	protected $id;
+	protected $requestParameters;
+	protected $request_url;
+	protected $headers;
+	protected $acceptType = 'application/atom+xml';
+	protected $mockResponseFilePath;
+	
+	protected $responseCode;
+	protected $responseOk = FALSE;
+	protected $eTag = null;
+	protected $responseBody;
+	protected $doc;
+	protected $atomTitle = null;
+	protected $atomLink = null;
+	
+	protected $errorCode = null;
+	protected $errorMessage = null;
+	protected $errorDetail = null;
+	
+	
+	
+	/**
+	 * Create standard getters and setters for all the properties
+	 * 
+	 * @param string $method
+	 * @param array $params
+	 * @throws \Exception
+	 */
+	
+	public function __call($method, $params) {
+		if (strlen($method) > 4) {
+			$action = substr($method, 0, 3);
+			$property = strtolower(substr($method, 3, 1)) . substr($method, 4);
+			if ($action == 'get' && property_exists($this, $property)) {
+				return $this->$property;
+			}
+				
+			if ($action == 'set' && property_exists($this, $property)) {
+				if (in_array($property, static::$invalidFields)) {
+					Throw new \Exception('This field is not edittable.');
+				}
+				if (empty($params[0])) {
+					Throw new \Exception('You must send a valid ' . $property);
+				} else {
+					$this->$property = $params[0];
+					return;
+				}
+				throw new \Exception($method . ' missing required parameter');
+			}
+		} else {
+			throw new \Exception('Call to Undefined Method/Class Function');
+		}
+	}
+	
+	/**
+	 * Construct an OCLC Resource Object
+	 * 
+	 * @param string $id
+	 * @param array $options
+	 * - wskey: an WSKey object with a key and secret
+	 * - accessToken: an AccessToken object
+	 * - user: a user object
+	 * - parameters: an array of query parameters
+	 * - acceptType: the media type to send in the HTTP Accept Header. eg. application/json, application/atom+xml
+	 * - mockResponseFilePath: the file path to a mock response you want to use for testing purposes
+	 */
+	public function __construct($id = null, $options = null)
+	{
+		if (isset($id)){
+	        $this->id = $id;
+	    }
+	    
+		if (is_array($options)) {
+			self::parseOptions($options);
+		}
+		if (isset($this->id) and (isset($this->wskey) || isset($this->accessToken) || isset($this->requestParameters['wskey']))){
+			self::get();
+		}		
+	}
+	
+	/**
+	 * Get a Resource via HTTP
+	 * 
+	 * @param array $options
+	 */
+	
+	public function get($options = null)
+	{
+		if (is_array($options)) {
+			self::parseOptions($options);
+		}
+		 
+		$this->method = 'GET';
+	
+		$this->request_url = static::buildRequestURL(__FUNCTION__, $this->id, $this->requestParameters);
+		 
+		$this->headers = array(
+				'Accept' => $this->acceptType
+		);
+		 
+		if (in_array('HMAC', static::$supportedAuthenticationMethods) || in_array('AccessToken', static::$supportedAuthenticationMethods)){
+			self::buildAuthorizationHeader();
+			$this->headers['Authorization'] = $this->authHeader;
+		}
+	
+		$httpOptions = array('mockResponseFilePath' => $this->mockResponseFilePath);
+	
+		self::parseResponse(static::makeHTTPRequest($this->method, $this->request_url, $this->headers, $httpOptions));
+		 
+	}
+	
+	/**
+	 * Parse options into object properties
+	 * @param array $options
+	 */
+	protected function parseOptions($options) {
+		
+		if (isset($options['accessToken'])){
+			$this->accessToken = $options['accessToken'];
+		}elseif (isset($options['wskey'])){
+			$this->wskey = $options['wskey'];
+		}
+	
+		if (isset($options['user'])) {
+			$this->user = $options['user'];
+		}
+	
+		if (isset($options['parameters'])) {
+			foreach ($options['parameters'] as $name => $value) {
+				$this->requestParameters[$name] = $value;
+			}
+		}
+		if (isset($options['acceptType'])){
+			$this->acceptType = $options['acceptType'];
+		}
+	
+		if (isset($options['mockResponseFilePath'])){
+			$this->mockResponseFilePath = $options['mockResponseFilePath'];
+		}
+	
+	}
+	/**
+	 * Build the full request URL with object id and parameters if necessary
+	 * 
+	 * @param string $method
+	 * @param string $id
+	 * @param array $parameters
+	 * @param string $action
+	 * @return string
+	 */
+	
+	private static function buildRequestURL($method, $id = null, $parameters = null, $action = null) {
+		$request_url = static::buildObjectController($method);
+	
+		if (isset($id)) {
+			$request_url .= '/' . $id;
+		}
+		if (isset($action)) {
+			$request_url .= '/' . $action;
+		}
+		$request_url .= self::buildQuery($parameters);
+	
+		return $request_url;
+	}
+	
+	/**
+	 * Build the base url for the object and the controller
+	 * 
+	 * @param string $method
+	 * @return string
+	 */
+	
+	private static function buildObjectController($method){
+		if (static::$dataURLsyntax){
+			if ($method == 'search') {
+				$request_url = static::$service_url . static::$object_path . '/search';
+			} else {
+				$request_url = static::$service_url . static::$object_path . '/data';
+			}
+		} else {
+			$request_url = static::$service_url . static::$object_path;
+		}
+		return $request_url;
+		 
+	}
+	
+	/**
+	 * Build the query string for the url
+	 * 
+	 * @param array $parameters
+	 * @return string
+	 */
+	private static function buildQuery($parameters = null)
+	{
+		$all_params = array();
+	
+		if (is_array($parameters)) {
+			$all_params = array_merge_recursive($all_params, $parameters);
+		}
+	
+		if (count($all_params) > 0) {
+			$querystring = '?' . http_build_query($all_params, '', '&');
+		} else {
+			$querystring = '';
+		}
+		return $querystring;
+	}
+	
+	/**
+	 * Build an Authorization Header based on information in object
+	 * @throws \Exception
+	 */
+	
+	private function buildAuthorizationHeader()
+	{
+		if (isset($this->accessToken)){
+			$this->authHeader = 'Bearer ' . $this->accessToken->getValue();
+			if (isset($this->user)){
+				$this->authHeader .= ', principalID="' . $this->user->getPrincipalID() . '", principalIDNS="' . $this->user->getPrincipalIDNS() . '"';
+			}
+		} elseif (isset($this->wskey)){
+			$options = array(
+					'user' => $this->user
+			);
+			$this->authHeader = $this->wskey->getHMACSignature($this->method, $this->request_url, $options);
+		} else {
+			Throw new \Exception('You must pass either a wskey or an accessToken Object in the options');
+		}
+	}
+	
+	/**
+	 * 
+	 * @param string $method
+	 * @param string $request_url
+	 * @param array $headers
+	 * @param array $options
+	 * @return \Guzzle\Http\Exception\BadResponseException
+	 */
+	
+	public static function makeHTTPRequest($method, $request_url, $headers, $options = null)
+	{   
+	    (isset($options['requestBody']) ? $requestBody = $options['requestBody'] : $requestBody = null);
+	    (isset($options['upload']) ? $upload = $options['upload'] : $upload = false);
+	    (isset($options['mockResponseFilePath']) ? $mockResponseFilePath = $options['mockResponseFilePath'] : $mockResponseFilePath = null);
+	    
+		if (defined('USER_AGENT')) {
+			$userAgent = USER_AGENT;
+		} else {
+			$userAgent = 'OCLC Platform PHP Library';
+		}
+		
+		$client = new Client();
+		$client->setDefaultOption('timeout', 60);
+		$client->setDefaultOption('redirect.strict', true);
+	
+		$history = new HistoryPlugin();
+		$history->setLimit(1);
+		$client->addSubscriber($history);
+		$client->setuserAgent($userAgent);
+		if (isset($mockResponseFilePath)) {
+			$plugin = new MockPlugin();
+			$client->addSubscriber($plugin);
+			$plugin->addResponse($mockResponseFilePath);
+		}
+		
+		if ($method == 'POST' and $upload == true) {
+			$request = $client->post($request_url, $headers, array(
+					'file_field' => $requestBody
+			));
+		} elseif ($method == 'POST' || $method =='PUT' || !empty($requestBody)) {
+			$request = $client->createRequest($method, $request_url, $headers, $requestBody);
+		} else {
+			$request = $client->createRequest($method, $request_url, $headers);
+		}
+		$request->getCurlOptions()->set(CURLOPT_SSL_VERIFYHOST, false);
+		$request->getCurlOptions()->set(CURLOPT_SSL_VERIFYPEER, false);
+		
+		try {
+			$response = $request->send();
+		} catch (\Guzzle\Http\Exception\BadResponseException $error) {
+			$response = $error;
+		}
+		return $response;
+	}
+	
+	/**
+	 * 
+	 * @param unknown $response
+	 */
+	protected function parseResponse($response) {
+		if (!is_a($response, '\Guzzle\Http\Exception\BadResponseException')) {
+			self::parseSuccess($response);
+		} else {
+			self::parseError($response->getResponse());
+		}
+	}
+	
+	/**
+	 * Parse the Guzzle Response into properties for successes
+	 * @param unknown $response
+	 */
+	
+	protected function parseSuccess($response) {
+		$this->responseCode = $response->getStatusCode();
+		$this->responseBody = $response->getBody(true);
+		$this->responseSuccessful = true;
+		$etag = $response->getETag();
+		if (isset($etag)) {
+			$this->eTag = $etag;
+		}
+			
+		//check to see if it is XML or not
+		libxml_use_internal_errors(true);
+		simplexml_load_string($response->getBody(true));
+			
+		if (empty($this->responseBody) || count(libxml_get_errors()) > 0) {
+			$isXML = FALSE;
+		} else {
+			$isXML = TRUE;
+		}
+		libxml_clear_errors();
+		if ($isXML) {
+			self::from_xml($response->getBody(true));
+		}
+	}
+	
+	/**
+	 * Parse the Guzzle Response into properties for errors
+	 * @param \Guzzle\Http\Response $response
+	 */
+	
+	protected function parseError($response){
+		$this->responseCode = $response->getStatusCode();
+		$this->responseBody = $response->getBody(true);
+		//check to see if it is XML or not
+		libxml_use_internal_errors(true);
+		simplexml_load_string($response->getBody(true));
+			
+		if (empty($this->responseBody) || count(libxml_get_errors()) > 0) {
+			$isXML = FALSE;
+		} else {
+			$isXML = TRUE;
+		}
+		libxml_clear_errors();
+		if ($isXML) {
+			$errors = simplexml_load_string($this->responseBody);
+			$errors->registerXPathNamespace("oclc", "http://worldcat.org/xmlschemas/response");
+			$code = $errors->xpath('//oclc:code');
+			$message = $errors->xpath('//oclc:message');
+			$detail = $errors->xpath('//oclc:detail');
+			if (isset($code[0])) {
+				$this->errorCode = (string)$code[0];
+			}
+			if (isset($message[0])){
+				$this->errorMessage = (string)$message[0];
+			}
+			if (isset($detail[0])){
+				$this->errorDetail = (string)$detail[0];
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * @param \OCLC\Search $search
+	 * @param unknown $response
+	 * @return \OCLC\Search
+	 */
+	protected static function parseSearchResponse($search, $response) {
+		if (!is_a($response, '\Guzzle\Http\Exception\BadResponseException')) {
+			$search->setResponseSuccessful($response->isSuccessful());
+			$search->setResponseCode($response->getStatusCode());
+			$search->setResponseBody($response->getBody(true));
+			$etag = $response->getETag();
+			if (!empty($etag)){
+				$search->setEtag($etag);
+			}
+			// figure out if it is Atom or not based on namespaces
+			$results = simplexml_load_string($search->getResponseBody());
+			$namespaces = $results->getNamespaces(true);
+		  
+			if (in_array('http://www.loc.gov/zing/srw/', $namespaces)) {
+				static::parseSRUResponse($results, $search);
+			} else {
+				static::parseAtomFeed($results, $search);
+			}
+	
+		} else {
+			$search->setResponseCode($response->getResponse()->getStatusCode());
+			$search->setResponseBody($response->getResponse()->getBody(true));
+		}
+	
+		return $search;
+	}
+	
+	/**
+	 * Parse the information in the Atom Feed and add each entry as an object into a result set array within the \OCLC\Search object
+	 * @param \SimpleXMLElement $results
+	 * @param \OCLC\Search $search_response
+	 */
+	
+	protected static function parseAtomFeed($results, $search_response) {
+		$results->registerXPathNamespace("atom", "http://www.w3.org/2005/Atom");
+		$results->registerXPathNamespace("os", "http://a9.com/-/spec/opensearch/1.1/");
+			
+		// want an array of resource objects with their XML
+		$entries = array();
+		foreach ($results->xpath('/atom:feed/atom:entry') as $entry) {
+			// create a new resource using class name
+			$class = get_called_class();
+			$resource = new $class;
+			$resource->from_xml($entry->saveXML());
+			$entries[] = $resource;
+		}
+		$search_response->setResultSet($entries);
+			
+		// set the currentPage
+		$currentPage = $results->xpath('/atom:feed/os:startIndex');
+		$search_response->setCurrentPage((string)$currentPage[0]);
+		//set the total results
+		$totalResults = $results->xpath('/atom:feed/os:totalResults');
+		$search_response->setTotalResults((string)$totalResults[0]);
+		// set the items per page
+		$itemsPerPage = $results->xpath('/atom:feed/os:itemsPerPage');
+		if ((string)$totalResults[0]  == 0) {
+			$itemsPerPage = 0;
+			$totalPages = 0;
+		} elseif((string)$totalResults[0] < (string)$itemsPerPage[0]) {
+			$itemsPerPage = (string)$totalResults[0];
+			$totalPages = 1;
+		} else {
+			$totalPages = (string)$totalResults[0] /(string)$itemsPerPage[0];
+			$itemsPerPage = (string)$itemsPerPage[0];
+		}
+		$search_response->setItemsPerPage((string)$itemsPerPage[0]);
+		// calculate and set the total # of pages
+		$search_response->setTotalPages($totalPages);
+	}
+	
+	/**
+	 * Parse the information in the SRU response and add each record as an object into a result set array the \OCLC\Search object
+	 * @param \SimpleXMLElement $results
+	 * @param \OCLC\Search $search_response
+	 */
+	
+	protected static function parseSRUResponse($results, $search_response) {
+		$results->registerXPathNamespace("sru", "http://www.loc.gov/zing/srw/");
+		// want an array of resource objects with their XML
+		$records = array();
+		foreach ($results->xpath('//sru:record/sru:recordData') as $record) {
+			$class = get_called_class();
+			$resource = new $class;
+			$resource->from_xml($record->saveXML());
+			$records[] = $resource;
+		}
+		$search_response->setResultSet($records);
+			
+		// set the currentPage
+		$currentPage = $results->xpath('//sru:startRecord');
+		if (count($currentPage) > 0) {
+			$search_response->setCurrentPage((string)$currentPage[0]);
+		}
+		//set the total results
+		$totalResults = $results->xpath('//sru:numberOfRecords');
+		$search_response->setTotalResults((string)$totalResults[0]);
+	
+		// set the items per page
+		$itemsPerPage = $results->xpath('//sru:maximumRecords');
+	
+		if ((string)$totalResults[0] == 0) {
+			$itemsPerPage = 0;
+			$totalPages = 0;
+		} elseif(count($itemsPerPage) > 0 && (string)$totalResults[0] < (string)$itemsPerPage[0]) {
+			$itemsPerPage = (string)$totalResults[0];
+			$totalPages = 1;
+		} elseif (count($itemsPerPage) > 0) {
+			$totalPages = (string)$totalResults[0] /(string)$itemsPerPage[0];
+			$itemsPerPage = (string)$itemsPerPage[0];
+		} elseif ((string)$totalResults[0] == 1) {
+			$itemsPerPage = 1;
+			$totalPages = 1;
+		}
+		$search_response->setItemsPerPage($itemsPerPage);
+		// calculate and set the total # of pages
+		$search_response->setTotalPages($totalPages);
+	}
+	
+	/**
+	 * Parse the object response either as an Atom Entry or other XML document
+	 * @param string $response
+	 */
+	
+	protected function from_xml($response) {
+		$this->responseBody = $response;
+	
+		$entry = simplexml_load_string($this->responseBody);
+	
+		if ($entry->getName() == 'entry') {
+			$namespaces = $entry->getNamespaces(true);
+				
+			if (isset($namespaces['gd'])) {
+				$gd = $entry->children($namespaces['gd']);
+	
+				//set Etag
+				if (isset($gd->etag)) {
+					$this->eTag = (string)$gd->etag;
+				} else {
+					$this->eTag = (string)$gd['etag'];
+				}
+			}
+			$this->atomTitle = (string)$entry->title ;
+			$this->atomLink = $entry->link['href'];
+			if (count($entry->xpath('//content/child::*')) > 0) {
+				$doc = $entry->xpath('//content/child::*');
+			} else {
+				$entry->registerXPathNamespace('atom', 'http://www.w3.org/2005/Atom');
+				$doc = $entry->xpath('//atom:content/child::*');
+			}
+			if (is_array($doc)) {
+				$doc = $doc[0];
+			}
+			$this->doc = $doc->asXML();
+			// parse any Atom error
+			if (isset($namespaces['oclc'])) {
+				$oclc = $entry->children($namespaces['oclc']);
+				$this->setErrorCode($oclc->error->code);
+				$this->setErrorMessage($oclc->error->message);
+				$this->setErrorDetail($oclc->error->detail);
+			}
+			$this->from_doc();
+		} else {
+			$this->doc = $response;
+			$this->from_doc();
+		}
+	}
+}
+
---
+++ tests/ResourceTestAccessToken.php
@@ -0,0 +1,54 @@
+<?php
+require('/Users/coombsk/Documents/workspace/code_libraries/oclc-auth.phar');
+require('/Users/coombsk/git/php-kb-rest-api/src/Resource.php');
+
+class ResourceTest extends PHPUnit_Framework_TestCase {
+
+	public function setup(){
+		Resource::$service_url = 'http://worldcat.org/webservices/kb/rest';
+		Resource::$object_path = '/settings';
+		Resource::$dataURLsyntax = false;
+		Resource::$supportedAuthenticationMethods = array('HMAC', 'AccessToken');
+	}
+	public function testConstructorWithID() {
+		$options = array(
+			'accessToken' => 'tk_fakefake',
+			'mockResponseFilePath' => __DIR__ . '/mocks/200.txt'
+		);
+		$resource = new Resource('123', $options);
+		
+		// Make sure the id is set
+		$this->assertAttributeEquals('123', 'id', $resource);
+
+		// make sure the parameters are set
+		$this->assertAttributeNotEmpty('requestParameters', $resource);
+		$parameters = $resource->getRequestParameters();
+		$this->assertEquals('myKey', $parameters['wskey']);
+		
+		// make sure the url gets built right
+		$this->assertAttributeEquals('http://worldcat.org/webservices/kb/rest/settings/123?wskey=myKey', 'request_url', $resource);
+		
+		//make sure mockResponseFilePath is set
+		$this->assertAttributeEquals(__DIR__ . '/mocks/200.txt', 'mockResponseFilePath', $resource);
+		
+		//make sure the authorization header is set right
+		$this->assertAttributeEquals('Bearer tk_fakefake', 'auth_header', $resource);
+		
+		//make sure the accept header is set right
+		$this->assertAttributeEquals('application/atom+xml', 'acceptType', $resource);
+		
+		//make sure the HTTP request returns a 200
+		$this->assertAttributeEquals('200', 'responseCode', $resource);
+		$this->assertAttributeNotEmpty('responseBody', $resource);
+		$this->assertAttributeEmpty('errorCode', $resource);
+		
+		return $resource;
+	}
+	
+	public function testSearch(){
+		
+	}
+	
+	
+
+}
